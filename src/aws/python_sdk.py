fc4c10f14087218a34614cd8b50b6b2842c0ff73
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2023-10-10 12:23:12 +0100

    feat: add new method to download comprehend results

--- src/aws/python_sdk.py
+++ src/aws/python_sdk.py
@@ -31,24 +31,24 @@ class AwsDf:
     def __init__(self):
         self.session_time = time()
 
-        self.aws = self.__create_session()
+        self.aws_session = self.__create_session()
 
     @verify_session(renew_session=__create_session)
     def get_bucket_as_df(self, bucket_link):
-        if not wr.s3.does_object_exist(bucket_link, boto3_session=self.aws):
+        if not wr.s3.does_object_exist(bucket_link, boto3_session=self.aws_session):
             raise Exception(f'No bucket found with this path: {bucket_link}')
 
-        return wr.s3.read_parquet(path=bucket_link, boto3_session=self.aws)
+        return wr.s3.read_parquet(path=bucket_link, boto3_session=self.aws_session)
 
     def get_s3_bucket_obj_list(self, bucket_link):
-        return wr.s3.list_objects(bucket_link, boto3_session=self.aws)
+        return wr.s3.list_objects(bucket_link, boto3_session=self.aws_session)
 
     @verify_session(renew_session=__create_session)
     def get_df_from_athena(self, query, db):
-        return wr.athena.read_sql_query(query, db, boto3_session=self.aws, ctas_approach=False)
+        return wr.athena.read_sql_query(query, db, boto3_session=self.aws_session, ctas_approach=False)
 
     @verify_session(renew_session=__create_session)
-    def upload_to_s3(self, df, bucket, name):
+    def upload_to_s3(self, df, bucket, name, ext="parquet"):
         bucket_path = f"s3://{bucket}/{name}"
 
         response = {}
@@ -57,12 +57,19 @@ class AwsDf:
             if response != {}:
                 sleep(2 * 60)
 
-            response = wr.s3.to_parquet(df, index=False, path=bucket_path, boto3_session=self.aws)
+            if ext == "csv":
+                response = wr.s3.to_csv(df, index=False, path=bucket_path, boto3_session=self.aws_session)
+            else:
+                response = wr.s3.to_parquet(df, index=False, path=bucket_path, boto3_session=self.aws_session)
 
         return response
 
+    def download_file_using_client(self, file_uri, file_name, output_path):
+        aws_client = client(
+            's3',
+            region_name=get_env_var('AWS_REGION_NAME', 'str'),
+            aws_access_key_id=get_env_var('AWS_ACCESS_KEY_ID', 'str'),
+            aws_secret_access_key=get_env_var('AWS_SECRET_ACCESS_KEY', 'str')
+        )
 
-
-
-
-
+        aws_client.download_file(file_uri, file_name, output_path)
